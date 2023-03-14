a3af678b6f0d4cef8d0ced5c4d20a15647a156b5
Leo Almeida <leowoa@outlook.com>
2023-03-14 16:02:00 -0300

    implemented a few buttons

--- src/main/java/com/example/calculadora/HelloController.java
+++ src/main/java/com/example/calculadora/HelloController.java
@@ -4,8 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 import static java.lang.Integer.parseInt;
@@ -63,17 +66,23 @@ public class HelloController {
 
     private void initialize(){
 
+        // TODO: 10/03/2023
+        //adicionar o metodo symbolremover (que retorna uma String) pra remover os simbolos no before antes de mandar pro formatador vai da certinho igual fiz no botão 1
 
     }
 
     public void plusCalc(ActionEvent actionEvent) {
         try{
-            boolean test = firstPut("+");
-            previousCalc("+");
-            System.out.println(previousCalc("+"));
-            if (!test){
-                checkZeroAndSet(calculator(getUpperNumber(), getLowerNumber(), "+"),"+");
+            if (isLowerEmpty()){
+                checkZeroAndSet(getUpperNumber(), "+");
+            } else {
+                boolean test = firstPut("+");
+                previousCalc("+");
+                if (!test){
+                    checkZeroAndSet(calculator(getUpperNumber(), getLowerNumber(), "+"),"+");
+                }
             }
+
         }catch (RuntimeException ignored){
         }
     }
@@ -83,7 +92,6 @@ public class HelloController {
         try{
             boolean test = firstPut("-");
             previousCalc("-");
-            System.out.println(previousCalc("-"));
             if (!test){
                 checkZeroAndSet(calculator(getUpperNumber(), getLowerNumber(), "-"),"-");
             }
@@ -95,7 +103,6 @@ public class HelloController {
         try{
             boolean test = firstPut("*");
             previousCalc("*");
-            System.out.println(previousCalc("*"));
             if (!test){
                 checkZeroAndSet(calculator(getUpperNumber(), getLowerNumber(), "*"),"*");
             }
@@ -103,8 +110,33 @@ public class HelloController {
         }
     }
 
+    public void equalButton(ActionEvent actionEvent) {
+        ArrayList<String> last = new ArrayList<>(List.of(upperDisplay.getText().split("")));
+        String operator = last.get(last.size() - 1);
+
+        double result = calculator(getUpperNumber(), getLowerNumber(), operator);
+        checkZeroAndSet(result);
+    }
+
+    public void percentage(ActionEvent actionEvent) {
+        ArrayList<String> last = new ArrayList<>(List.of(upperDisplay.getText().split("")));
+        String operator = last.get(last.size() - 1);
+
+        if (!isLowerEmpty() && !isUpperEmpty()){
+            double result = (getUpperNumber() * getLowerNumber()) / 100;
+
+            checkZeroAndSet(calculator(getUpperNumber(), result, operator));
+        }
+    }
+
+
     public void inverter(ActionEvent actionEvent) {
-        lowerDisplay.setText(String.valueOf(getLowerNumber() * -1)); //falta adicionar um modo de não ir o 5.0 se não precisar
+        if (String.valueOf(getLowerNumber()).endsWith(".0")){
+            lowerDisplay.setText(String.valueOf(Math.round(getLowerNumber() * -1)));
+
+        } else {
+            lowerDisplay.setText(String.valueOf(getLowerNumber() * -1));
+        }
     }
 
 
@@ -113,7 +145,10 @@ public class HelloController {
     public void buttonOneAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 1);
+            before += "1";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -122,7 +157,10 @@ public class HelloController {
     public void buttonTwoAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 2);
+            before += "2";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -131,7 +169,10 @@ public class HelloController {
     public void buttonThreeAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 3);
+            before += "3";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -140,7 +181,10 @@ public class HelloController {
     public void buttonFourAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 4);
+            before += "4";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -149,7 +193,10 @@ public class HelloController {
     public void buttonFiveAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 5);
+            before += "5";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -158,7 +205,10 @@ public class HelloController {
     public void buttonSixAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 6);
+            before += "6";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -167,7 +217,10 @@ public class HelloController {
     public void buttonSevenAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 7);
+            before += "7";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -176,7 +229,10 @@ public class HelloController {
     public void buttonEightAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 8);
+            before += "8";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -185,7 +241,10 @@ public class HelloController {
     public void buttonNineAction(ActionEvent actionEvent) {
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 9);
+            before += "9";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
         }
@@ -194,20 +253,22 @@ public class HelloController {
     public void buttonZeroAction(ActionEvent actionEvent){
         String before = lowerDisplay.getText();
         if (before.length() < 9){
-            lowerDisplay.setText(before + 0);
+            before += "0";
+            before = symbolRemover(before);
+            before = NumberFormat.getInstance(Locale.GERMAN).format(Integer.parseInt(before));
+            lowerDisplay.setText(before);
         } else {
             lowerDisplay.setText(before + "");
-
         }
     }
 
     public void buttonComaAction(ActionEvent actionEvent){
         String before = lowerDisplay.getText();
         if (before.length() < 8){
-            if (before.contains(".")){
+            if (before.contains(",")){
                 lowerDisplay.setText(before + "");
             } else {
-                lowerDisplay.setText(before + ".");
+                lowerDisplay.setText(before + ",");
             }
 
         } else {
@@ -217,7 +278,7 @@ public class HelloController {
 
     public String symbolRemover (String text){
         List<String> array = new ArrayList<>(List.of(text.split("")));
-        array.removeIf(a -> Objects.equals(a, "+") || Objects.equals(a, "-") || Objects.equals(a, "=")|| Objects.equals(a, " ") || Objects.equals(a, "/") || Objects.equals(a, "*") || Objects.equals(a, "%"));
+        array.removeIf(a -> Objects.equals(a, "+") || Objects.equals(a, "-") || Objects.equals(a, "=")|| Objects.equals(a, " ") || Objects.equals(a, "/") || Objects.equals(a, "*") || Objects.equals(a, "%") || Objects.equals(a, "."));
         String result = String.join("", array);
         return result;
     }
@@ -226,6 +287,11 @@ public class HelloController {
     public void buttonEraseAction(ActionEvent actionEvent) {
         lowerDisplay.clear();
         upperDisplay.clear();
+    }
+
+    public void buttonEraseLower(ActionEvent actionEvent) {
+        lowerDisplay.clear();
+
     }
 
     private double getLowerNumber(){
@@ -271,11 +337,21 @@ public class HelloController {
     }
 
     private void checkZeroAndSet(double result, String operator){
-        if (String.valueOf(result).endsWith(".0")){
-            upperDisplay.setText(Math.round(result) + operator);
+        if (String.valueOf(result).endsWith(",0")){
+            upperDisplay.setText(NumberFormat.getInstance(Locale.GERMAN).format(Math.round(result)) + operator);
             lowerDisplay.clear();
         } else {
-            upperDisplay.setText(result + operator);
+            upperDisplay.setText(NumberFormat.getInstance(Locale.GERMAN).format(result) + operator);
+            lowerDisplay.clear();
+        }
+    }
+
+    private void checkZeroAndSet(double result){
+        if (String.valueOf(result).endsWith(",0")){
+            upperDisplay.setText(NumberFormat.getInstance(Locale.GERMAN).format(Math.round(result)));
+            lowerDisplay.clear();
+        } else {
+            upperDisplay.setText(NumberFormat.getInstance(Locale.GERMAN).format(Math.round(result)));
             lowerDisplay.clear();
         }
     }
